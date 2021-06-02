ea0776deec11397651b556d8e7848c764c37f1db
Vasyl Habalevych <vasylhab@gmail.com>
2021-06-02 02:13:58 +0200

    updated form and menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const toggler = document.querySelector('.header__toggler');
+const page = document.querySelector('.page');
+const nav = document.querySelector('.nav');
+const menuIcon = document.querySelector('.header__menu-icon');
+
+const toggleMenu = () => {
+  page.classList.toggle('page--fixed');
+  nav.classList.toggle('nav--active');
+  menuIcon.classList.toggle('header__menu-icon--active');
+};
+
+toggler.addEventListener('click', toggleMenu);
+nav.addEventListener('click', toggleMenu);
